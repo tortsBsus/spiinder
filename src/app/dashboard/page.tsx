28eaa1214c4e238342b1bd47e951f20e3b452cc6
tortsBsus <meganraj251@gmail.com>
2024-07-01 18:53:11 +0530

    redirect to dashboard post login
    also detect if already signed in

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,31 +1,69 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import { UserAuth } from '../context/AuthContext';
-import { Progress } from "@/components/ui/progress"
+import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
+import { useRouter } from 'next/navigation';
 
- 
+
+
 function Dashboard() {
 
-  const {user} = UserAuth();
+  const { user, googleSignOut } = UserAuth();
   const [loading, setLoading] = useState(true);
+  const navigate = useRouter();
+
+  
+  const handleSignOut = async () => {
+    try {
+      await googleSignOut()
+    }
+    catch (error) {
+      console.log(error);
+    }
+
+    navigate.push("/");
+
+  }
+
 
   useEffect(() => {
     const checkAuthentication = async () => {
-      await new Promise((resolve)=>setTimeout(resolve, 1000));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
       setLoading(false);
     }
-  checkAuthentication();
+    checkAuthentication();
   }, [user]);
-  
+
+
+
+
+
 
   return (
-    <div className="bg-gradient-to-br from-[#9EF01A] to-[#38B000] h-screen w-screen p-5 md:p-20">
-      {loading? (<Progress value={33} />) : !user ? <p>This is a protected page, login to see this page</p> :
-      <p>{user.displayName}&apos;s dashboard</p>
-      }
-      
+    <div className="bg-gradient-to-br from-[#9EF01A] to-[#38B000] min-h-screen w-screen p-5 md:p-20">
+
+      {user ?  <Button variant="outline" className="w-1/2 md:w-1/4 text-wrap" onClick={() => handleSignOut()}>Sign Out {user.displayName}!</Button> : <p>Login to see this page </p>}
+
+      {/* {loading? (<Progress value={33} />) : !user ? <p>This is a protected page, login to see this page</p> :
+      <div>
+        <h1>{user.displayName}&apos;s dashboard</h1>
+        <h2>Upload a CSV file where transactions are formatted as shown in the example</h2>
+        <Button variant="outline" className="w-1/2 md:w-1/4 text-wrap" onClick={() => alert("Uploading file")}>Upload a CSV file </Button>
       </div>
+      } */}
+
+     
+
+    </div>
   )
 }
 
 export default Dashboard
+
+
+
+
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,35 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { UserAuth } from './context/AuthContext';
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter
+} from "@/components/ui/dialog"
+
+
 
 
 
 export default function Home() {
   const { user, googleSignIn, googleSignOut } = UserAuth();
   const [loading, setLoading] = useState(true);
+  const navigate = useRouter();
+  const [myTrigger, setMyTrigger] = useState(false);
+  // useEffect(() => {
+
+  //   if (user) {
+  //     console.log(user);
+  //     navigate.push("/dashboard");
+
+  //   }
+
+  // }, [user]);
 
   const handleSignIn = async () => {
     try {
@@ -17,6 +40,8 @@ export default function Home() {
     catch (error) {
       console.log(error);
     }
+
+    setMyTrigger(true);
 
   }
 
@@ -33,13 +58,14 @@ export default function Home() {
 
   useEffect(() => {
     const checkAuthentication = async () => {
-      await new Promise((resolve)=>setTimeout(resolve, 10000));
+      if(user)setMyTrigger(true);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
       setLoading(false);
     }
-  checkAuthentication();
+    checkAuthentication();
   }, [user]);
 
-  
+
 
   return (
     <div className="bg-gradient-to-br from-[#9EF01A] to-[#38B000] h-screen w-screen p-5 md:p-20">
@@ -47,12 +73,26 @@ export default function Home() {
       <div className="h-full w-full flex flex-col justify-center items-center bg-black/35 rounded-lg p-2">
         <h1 className="text-2xl md:text-6xl font-extrabold text-white text-center">Categorise your spends via swipes</h1>
         <h2 className="text-sm md:text-xl text-center font-medium text-white my-2 md:my-4 md:w-1/2"> an expense tracker that lets you swipe your transactions into different categories and understand where your money goes</h2>
-    {!user ? 
-        <Button variant="outline" className="w-1/2 md:w-1/4" onClick={() => handleSignIn()}>Let&apos;s begin!</Button> :
-        
-        <Button variant="outline" className="w-1/2 md:w-1/4 text-wrap" onClick={() => handleSignOut()}>Sign Out {user.displayName}!</Button>
-    }
-    
+        {!user ?
+          <Button variant="outline" className="w-1/2 md:w-1/4" onClick={() => handleSignIn()}>Let&apos;s begin!</Button> :<>
+          <Dialog open = {myTrigger} onOpenChange={setMyTrigger}>
+            <DialogTrigger/>
+            <DialogContent className="bg-[#38B000] border-[#38B000] border-2 border-solid text-white">
+              <DialogHeader>
+                <DialogTitle>Welcome back, {user ? user.displayName : " "} !</DialogTitle>
+                <DialogDescription className="text-white">
+                  You are being redirected to your dashboard now!
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <Button className="bg-[#0e4118] shadow-lg border-[#38B000] border-2 border-solid text-white" type="submit" onClick = { ()=> navigate.push("/dashboard") }>Take me to the dashboard!</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          <Button variant="outline" className="w-1/2 md:w-1/4" onClick={()=> navigate.push("/dashboard")}>Go to Dashboard!</Button>
+          </>
+        }
+
       </div>
     </div>
   );
