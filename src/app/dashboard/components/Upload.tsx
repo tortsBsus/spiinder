4ffaa3c60cc5c7bb4054dbde65dce4d955502c17
tortsBsus <meganraj251@gmail.com>
2024-07-08 18:47:18 +0530

    add example option for uploads

--- src/app/dashboard/components/Upload.tsx
+++ src/app/dashboard/components/Upload.tsx
@@ -1,53 +1,144 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Papa from 'papaparse';
 import { Button } from "@/components/ui/button";
 import { Input } from '@/components/ui/input';
+import ExampleFile from "./Example";
 
-interface UploadProps{
-    onStepChange : (step:number) => void;
-    updateCSV : (temp:object[]) => void;
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+
+
+
+interface UploadProps {
+  onStepChange: (step: number) => void;
+  updateCSV: (temp: object[]) => void;
 }
-const Upload : React.FC<UploadProps> = ({ onStepChange, updateCSV }) => {
+const Upload: React.FC<UploadProps> = ({ onStepChange, updateCSV }) => {
 
-    const [error, setError] = useState(null);
+  const [error, setError] = useState(null);
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0]; // Access the selected file
-      
-        if (!file) return; // Handle no file selected
-      
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          const result = e.target?.result as string;
-          if (!result) return; // Handle empty result if any
-      
-          const parsedData:any = Papa.parse(result, { header: true }).data;
-          updateCSV(parsedData); // Update CSV data in parent component
-          console.log(parsedData.length); // Output parsed data length
-          setError(null); // Clear any previous errors
-        };
-        reader.onerror = (err:any) => {
-          setError(err.message); // Handle file read error
-        };
-        reader.readAsText(file); // Read file as text
-      };
+  const [uploadMethod, setUploadMethod] = useState("");
 
-    return (
-        <div className='border-white border-2 border-solid p-10 rounded-lg bg-white'>
-           <div className=' my-10'>
-        <h1 className='font-bold text-xl'>Upload Interface</h1>
-        <h2>Upload your CSV file that is in the specified format</h2>
-        </div>
-            <div>
-                <Input id="BudgetFile" type="file" accept=".csv" onChange={handleFileChange} />
-                {error && <p>{error}</p>}
-            </div>
-            <Button className='my-5' onClick={() => onStepChange(2)}> Move to the next step </Button>
+  const [receivedData, setReceivedData] = useState<any[]>([]);
+
+  const [myCSVTrigger, setMyCSVTrigger] = useState(false);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]; // Access the selected file
+
+    if (!file) return; // Handle no file selected
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const result = e.target?.result as string;
+      if (!result) return; // Handle empty result if any
+
+      const parsedData: any = Papa.parse(result, { header: true }).data;
+      setReceivedData(parsedData); 
+      console.log(parsedData);
+      setError(null); // Clear any previous errors
+    };
+    reader.onerror = (err: any) => {
+      setError(err.message); // Handle file read error
+    };
+    reader.readAsText(file); // Read file as text
+  };
+
+  const checkUpload = () => {
+
+    if(receivedData.length!=0){
+      console.log(receivedData);
+      updateCSV(receivedData);
+      onStepChange(2);
+    }
+    else alert("Nothing to be categorised!");
+  }
 
 
-        </div>
-    )
+
+  useEffect(() => {
+
+    switch (uploadMethod) {
+      case "Sheets": console.log(uploadMethod); break;
+      case "Statement":  console.log(uploadMethod); break;
+      case "CSV": setMyCSVTrigger(true); console.log(uploadMethod);
+        break;
+      case "Example":setReceivedData(ExampleFile); console.log(uploadMethod); break;
+      default: console.log("Invalid choice");
+    }
+
+  }, [uploadMethod])
+
+  return (
+    <>
+      <div className='flex flex-col md:flex-row justify-center  w-full'>
+        {/* ------------------------------------------------------------- Upload CSV ------------------------------------------------------------- */}
+        <Card className={`m-3 grow  ${uploadMethod == "CSV" ? "bg-[#38B000]/70 border-2 border-solid border-[#38B000]" : "hover:-translate-y-2 hover:translate-x-1 hover:bg-slate-200"}`} onClick={() => setUploadMethod("CSV")}>
+          <CardHeader>
+            <CardTitle>Upload CSV</CardTitle>
+            <CardDescription className={`${uploadMethod == "CSV" && "text-slate-900"}`}>Upload your CSV file that is in the specified format</CardDescription>
+          </CardHeader>
+          <CardContent>
+            
+          </CardContent>
+        </Card>
+        {/* ------------------------------------------------------------- Google Sheets ------------------------------------------------------------- */}
+        {/* <Card className={`m-3 grow  ${uploadMethod == "Sheets" ? "bg-[#38B000]/70 border-2 border-solid border-[#38B000]" : "hover:-translate-y-2 hover:translate-x-1 hover:bg-slate-200"}`} onClick={() => setUploadMethod("Sheets")}>
+          <CardHeader>
+            <CardTitle>Google Sheets</CardTitle>
+            <CardDescription className={`${uploadMethod == "Sheets" && "text-slate-900"}`}>Connect a spreadsheet with your data</CardDescription>
+          </CardHeader>
+
+        </Card> */}
+        {/* ------------------------------------------------------------- Example ------------------------------------------------------------- */}
+        <Card className={`m-3 grow  ${uploadMethod == "Example" ? "bg-[#38B000]/70 border-2 border-solid border-[#38B000]" : "hover:-translate-y-2 hover:translate-x-1 hover:bg-slate-200"}`} onClick={() => setUploadMethod("Example")}>
+          <CardHeader>
+            <CardTitle>Example File</CardTitle>
+            <CardDescription className={`${uploadMethod == "Example" && "text-slate-900"}`}>Try Spiinder using a sample file!</CardDescription>
+          </CardHeader>
+        </Card>
+        {/* ------------------------------------------------------------- PDF ------------------------------------------------------------- */}
+        {/* <Card className={`m-3 grow  ${uploadMethod=="Statement" ? "bg-[#38B000]/70 border-2 border-solid border-[#38B000]" : "hover:-translate-y-2 hover:translate-x-1 hover:bg-slate-200"}`} onClick={()=>setUploadMethod("Statement")}>
+          <CardHeader>
+            <CardTitle>PDF Bank Statement</CardTitle>
+            <CardDescription className={`${uploadMethod=="Statement" && "text-slate-900"}`}>Upload your Bank statement and it will be parsed automatically!</CardDescription>
+          </CardHeader>
+        </Card> */}
+      </div>
+      <Dialog open = {myCSVTrigger} onOpenChange={setMyCSVTrigger}>        
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Upload CSV</DialogTitle>
+            <DialogDescription>
+              Upload a CSV file from your device that adheres to this format
+            </DialogDescription>
+          </DialogHeader>
+<Input id="BudgetFile" type="file" accept=".csv" onChange={handleFileChange} />
+          {error && <p>{error}</p>}
+        </DialogContent>
+      </Dialog>
+
+     {receivedData.length != 0 && <Button className='m-5 w-full bg-[#38B000]' onClick={() => checkUpload()}> Move to the next step </Button> } 
+    </>
+  )
 }
 
 export default Upload
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -27,10 +27,10 @@ function Dashboard() {
 
   const { user, googleSignOut } = UserAuth();
   const [loading, setLoading] = useState<boolean>(true);
-  const [step, setStep] = useState<number>(0);
+  const [step, setStep] = useState<number>(1);
   const navigate = useRouter();
   const [csvData, setCsvData] = useState<Object>({});
-  const [categories, setCategories] = useState<string[]>(["Cat1","Cat2","Cat3","Cat4"])
+  const [categories, setCategories] = useState<string[]>(["Cat1", "Cat2", "Cat3", "Cat4"])
 
 
   const handleSignOut = async () => {
@@ -50,11 +50,11 @@ function Dashboard() {
     setStep(step);
   }
 
-  const updateCSVdata = (data:Object) => {
+  const updateCSVdata = (data: Object) => {
     setCsvData(data);
   }
 
-  const updateCategories = (newCats:string[]) => {
+  const updateCategories = (newCats: string[]) => {
     setCategories(newCats);
   }
 
@@ -66,6 +66,9 @@ function Dashboard() {
     checkAuthentication();
   }, [user]);
 
+useEffect(() => {
+  console.log(step);  
+}, [step])
 
 
 
@@ -74,8 +77,7 @@ function Dashboard() {
 
   return (
 
-    <div className="bg-gradient-to-br from-[#9EF01A] to-[#38B000] min-h-screen w-screen p-2 md:px-10 md:pt-5">
-
+    <div className="bg-[#06af94] min-h-screen w-screen p-2 md:px-10 md:pt-5">
 
 
 
@@ -84,92 +86,98 @@ function Dashboard() {
 
           <p>This is a protected page, login to see this page</p> :
           // Dashboard ------------------------------------------------------------------------------------------------------------------------------------------>
-          <div className='flex flex-col border-white border-2 border-solid min-h-screen justify-between rounded-lg bg-white p-5'>
-            {/* ------------------------------------------------------------- Header ------------------------------------------------------------- */}
-            <div>
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem>
-                    <BreadcrumbLink href="/">Begin</BreadcrumbLink>
-                  </BreadcrumbItem>
+          <div className='flex flex-col border-white border-2 border-solid h-screen justify-between rounded-lg bg-white p-5'>
+            {/* ------------------------------------------------------------- Breadcrumb ------------------------------------------------------------- */}
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/">Begin</BreadcrumbLink>
+                </BreadcrumbItem>
 
-                  {step > 0
-                    && (
-                      step == 1 ?
-                        <>
-                          <BreadcrumbSeparator />
-                          <BreadcrumbItem>
-                            <BreadcrumbPage>Upload</BreadcrumbPage>
-                          </BreadcrumbItem>
-                        </> : <>
-                          <BreadcrumbSeparator />
-                          <BreadcrumbItem>
-                            <BreadcrumbLink onClick={() => setStep(1)}>Upload</BreadcrumbLink>
-                          </BreadcrumbItem>
-                        </>)}
-                  {step > 1 && (step == 2 ?
-                    <>
-                      <BreadcrumbSeparator />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Categories</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </> : <>
-                      <BreadcrumbSeparator />
-                      <BreadcrumbItem>
-                        <BreadcrumbLink onClick={() => setStep(2)}>Categories</BreadcrumbLink>
-                      </BreadcrumbItem>
-                    </>)}
-                  {step > 2 && (step == 3 ?
-                    <>
-                      <BreadcrumbSeparator />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Swipe</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </> : <>
-                      <BreadcrumbSeparator />
-                      <BreadcrumbItem>
-                        <BreadcrumbLink onClick={() => setStep(3)}>Swipe</BreadcrumbLink>
-                      </BreadcrumbItem>
-                    </>)}
-                    {step > 3 && (step == 4 ?
-                    <>
-                      <BreadcrumbSeparator />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Graphs</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </> : <>
-                      <BreadcrumbSeparator />
-                      <BreadcrumbItem>
-                        <BreadcrumbLink onClick={() => setStep(4)}>Graphs</BreadcrumbLink>
-                      </BreadcrumbItem>
-                    </>)}
-                </BreadcrumbList>
-              </Breadcrumb>
+                {step > 0
+                  && (
+                    step == 1 ?
+                      <>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                          <BreadcrumbPage>Upload</BreadcrumbPage>
+                        </BreadcrumbItem>
+                      </> : <>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                          <BreadcrumbLink onClick={() => setStep(1)}>Upload</BreadcrumbLink>
+                        </BreadcrumbItem>
+                      </>)}
+                {step > 1 && (step == 2 ?
+                  <>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Categories</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </> : <>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbLink onClick={() => setStep(2)}>Categories</BreadcrumbLink>
+                    </BreadcrumbItem>
+                  </>)}
+                {step > 2 && (step == 3 ?
+                  <>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Swipe</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </> : <>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbLink onClick={() => setStep(3)}>Swipe</BreadcrumbLink>
+                    </BreadcrumbItem>
+                  </>)}
+                {step > 3 && (step == 4 ?
+                  <>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Graphs</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </> : <>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbLink onClick={() => setStep(4)}>Graphs</BreadcrumbLink>
+                    </BreadcrumbItem>
+                  </>)}
+              </BreadcrumbList>
+            </Breadcrumb>
+            <div className='flex justify-between align-middle items-center my-2'>
+
               {/* ------------------------------------------------------------- Title ------------------------------------------------------------- */}
-              <h1 className='text-xl font-bold mt-2' >{user.displayName}&apos;s dashboard</h1>
+              <div className='w-2/3'>
+                <h1 className='text-xl font-bold mt-2' >{user.displayName}&apos;s dashboard</h1>
+              </div>
+
+              {/* ------------------------------------------------------------- Footer ------------------------------------------------------------- */}
+              <div className='flex flex-col justify-end align-end items-end h-full w-1/3 '>
+                <Button variant="outline" className="md:w-full text-wrap bg-[#1fab8c]  " onClick={() => handleSignOut()}>Sign Out?</Button>
+              </div>
             </div>
 
+
             {/* ------------------------------------------------------------- Content ------------------------------------------------------------- */}
-            <div className='bg-[#38B000]/80 max-h-full max-w-full rounded-lg grow p-5 flex flex-col items-center justify-center'>
-              {step == 0 && <>
-                <Button onClick={() => setStep(1)}> Start new project </Button>
-              </>}
+            <div className='bg-[#21a095]/80 max-h-full max-w-full rounded-lg grow p-5 flex flex-col items-center justify-center'>
+
               {step == 1 && <>
-                <Upload onStepChange={updateSteps} updateCSV = {updateCSVdata} />
+                <Upload onStepChange={updateSteps} updateCSV={updateCSVdata} />
               </>}
-              
+
               {step == 2 && <>
                 <Categories onStepChange={updateSteps} updateCategories={updateCategories} />
               </>}
               {step == 3 && <>
-                <Swipe onStepChange={updateSteps} updateCSV = {updateCSVdata} data={csvData} categories={categories} />
+                <Swipe onStepChange={updateSteps} updateCSV={updateCSVdata} data={csvData} categories={categories} />
               </>}
               {step == 4 && <>
                 <Display onStepChange={updateSteps} csvData={csvData} />
               </>}
             </div>
-            {/* ------------------------------------------------------------- Footer ------------------------------------------------------------- */}
-            <Button variant="outline" className="max-w-full text-wrap bg-[#38B000] mt-5" onClick={() => handleSignOut()}>Sign Out as {user.displayName}?</Button>
+
           </div>
       }
 
